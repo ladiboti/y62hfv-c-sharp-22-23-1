3976b37a1c720404dc2b0485d85895e8b4426882
Ladi Botond <ladiboti@gmail.com>
2022-09-17 23:45:50 +0200

    garbage collector tester

--- Program.cs
+++ Program.cs
@@ -1,9 +1,24 @@
 ﻿using System;
 
-namespace hello_world {
-  internal class Program {
+namespace lesson1 {
+  class Tester {
+    public static int counter = 0;
+    public Tester() {
+      counter++;
+      Console.WriteLine(counter);
+    }
+
+    ~Tester() {
+      counter = 0;
+    }
+  }
+  class Program {
     static void Main(string[] args) {
-      Console.WriteLine("Hello World!");
+      while (true) {
+        Tester t = new Tester();
+        if (Tester.counter == 0)
+          break;
+      }
     }
   }
 }
