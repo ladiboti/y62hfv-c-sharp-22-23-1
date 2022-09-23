c4ff9260a9c8acc67046e6f3a1104265fa543a92
Ladi Botond <ladiboti@gmail.com>
2022-09-23 22:04:35 +0200

    last commit, functions renamed, base project finished

--- Program.cs
+++ Program.cs
@@ -5,7 +5,7 @@ namespace linq_playground {
   class Program {
     static void Main(string[] args) {
       OldSchoolSolver o1 = new OldSchoolSolver(5000, 0, 10000, true);
-      LinqSolver      l1 = new LinqSolver(5000, 0, 10000, true);
+      LinqSolver      l1 = new LinqSolver(5000, 0, 10000, true) ;
     }
   }
 }
